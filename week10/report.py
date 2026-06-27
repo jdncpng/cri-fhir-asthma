4461d2d2adf9c3233dae4e8721d88f2ee822cfbd
Lenka2136 <69755189+Lenka2136@users.noreply.github.com>
2026-06-27 13:32:48 +0200

    week10

---
+++ week10/report.py
@@ -0,0 +1,44 @@
+###################################################################
+# Lecture: Clinical Research Informatics - Sommer Semester 2026
+
+# Exercise Sheet: Week 10 - Data Analysis Report
+
+# | Student Name     | Matriculation Number |
+# | ---------------- | -------------------- |
+# | LENKA TUSCHEROVA | 631168               |
+# | OLEKSANDR SHKIL  | 657708               |
+# | JAEDEN CAPINIG   | 656852               |
+# | DAMIAN RYCHLICKI | 572581               |
+#
+# Additional information:
+#   used additional packages: {Package name and version}
+#   dataset used: {dataset or file name}
+###################################################################
+
+# Please add this header to all your submissions and adapt it to your information and submission.
+# The following structure is a recommendation, but not mandatory to use
+
+# --------------------------------------------------
+# Functions
+# --------------------------------------------------
+
+
+# --------------------------------------------------
+# Main Script
+# --------------------------------------------------
+
+# Week 4 - maybe
+
+# Week 5 L
+
+# Week 6 
+
+# Week 7 L
+
+# Week 8 
+
+# Week 9 L
+
+
+
+
